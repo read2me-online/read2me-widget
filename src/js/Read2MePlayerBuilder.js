48cb6397c0191b7ee54a3cdaa8b7a99275ef88be
ninoskopac <nino.skopac@gmail.com>
2017-12-03 22:53:12 +0700

    Fixed aforementioned bug

--- src/js/Read2MePlayerBuilder.js
+++ src/js/Read2MePlayerBuilder.js
@@ -11,27 +11,49 @@ class Read2MePlayerBuilder {
     static _replaceBlueprintWithPlayer(elem) {
         let appId = 2; // @TODO
         let url = elem.getAttribute('data-url');
-        let autoplay = elem.getAttribute('data-autoplay');
-        let sections = elem.getAttribute('data-sections');
-        let title = elem.getAttribute('data-title');
-        let thumbnail = elem.getAttribute('data-thumbnail');
+        let autoplay = elem.getAttribute('data-autoplay'); // @TODO
+        let cssSelectors = elem.getAttribute('data-css-selectors');
+        let title = elem.getAttribute('data-title'); // @TODO
+        let thumbnail = elem.getAttribute('data-thumbnail'); // @TODO
         let ignoreContentChange = elem.getAttribute('data-ignore-content-change');
 
-        if (sections) {
-            sections = sections.substring(1, sections.length - 1).split(',');
-        }
+        autoplay = this._booleanStringToBoolean(autoplay);
+        cssSelectors = this._cssSelectorsStringToArray(cssSelectors);
+        ignoreContentChange = this._booleanStringToBoolean(ignoreContentChange);
 
-        if (ignoreContentChange === 'true')
-            ignoreContentChange = true;
-        else if (ignoreContentChange === 'false')
-            ignoreContentChange = false;
-        else
-            throw 'Unsupported value for ignoreContentChange (can be "true" or "false")';
-
-        // console.log(appId, url, autoplay, sections, title, thumbnail, ignoreContentChange);
-
-        const player = new Read2MePlayer(appId, url, sections, ignoreContentChange);
+        const player = new Read2MePlayer(appId, url, cssSelectors, ignoreContentChange);
         player.render();
+    }
+
+    static _cssSelectorsStringToArray(cssSelectors) {
+        if (!cssSelectors)
+            return;
+
+        cssSelectors = cssSelectors.substring(1, cssSelectors.length - 1).split(',');
+        cssSelectors = cssSelectors.map(selector => selector.trim());
+        cssSelectors = cssSelectors.map(selector => this._trimByChar(selector, '"'));
+        cssSelectors = cssSelectors.map(selector => this._trimByChar(selector, "'"));
+
+        return cssSelectors;
+    }
+
+    static _booleanStringToBoolean(value) {
+        if (value === 'true')
+            value = true;
+        else if (value === 'false')
+            value = false;
+        else
+            throw 'Unsupported value for ignoreContentChange (can be "true" or "false", as a string)';
+
+        return value;
+    }
+
+    // https://stackoverflow.com/a/43333491/1325575
+    static _trimByChar(string, character) {
+        const first = [...string].findIndex(char => char !== character);
+        const last = [...string].reverse().findIndex(char => char !== character);
+
+        return string.substring(first, string.length - last);
     }
 }
 
