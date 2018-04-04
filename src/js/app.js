bc3b7db596ca1fe1226118d7de4140a94b9b7536
ninoskopac <nino.skopac@gmail.com>
2018-04-04 15:31:12 +0700

    now exposing key classes

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 import Read2MeHelpers from './Read2MeHelpers'
 import Read2MePlayerBuilder from "./Read2MePlayerBuilder";
 import Read2MePolyfills from "./Read2MePolyfills";
+import Read2MeBackendWrapper from "./Read2MeBackendWrapper";
+import Read2MeAnalyticsBackendWrapper from "./Read2MeAnalyticsBackendWrapper";
 
 Read2MeHelpers.documentReady(() => {
     if (Read2MeHelpers.isIe())
@@ -10,4 +12,10 @@ Read2MeHelpers.documentReady(() => {
     Read2MePolyfills.ArrayFrom();
     Read2MePolyfills.findIndex();
     const playerBuilder = new Read2MePlayerBuilder();
+
+    window.Read2Me = {
+        PlayerInstances: playerBuilder.playerInstances,
+        BackendWrapper: Read2MeBackendWrapper,
+        AnalyticsBackendWrapper: Read2MeAnalyticsBackendWrapper
+    };
 });
